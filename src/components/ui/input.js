54aa73aa30240dc49d3642ac3256cdce66c63088
executivefunk <nate0670@gmail.com>
2025-03-04 23:03:10 -0800

    Create input.js

---
+++ src/components/ui/input.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Input({ placeholder, value, onChange }) {
+  return (
+    <input
+      type="text"
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className="border p-2 rounded w-full"
+    />
+  );
+}
