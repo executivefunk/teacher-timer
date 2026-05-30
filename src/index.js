c0bb7b1eeda2e01acdc80184400ea2e8f1ad35cb
executivefunk <nate0670@gmail.com>
2026-05-29 20:17:56 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import "./index.css";
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app"; // Ensure this is lowercase
+import { createRoot } from "react-dom/client";
+import App from "./app";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
